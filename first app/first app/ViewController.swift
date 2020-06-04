c873c6bdded0ff33b811074a61cd2c3ab6788470
@gastby <gastby@gastbydeMacBook-Pro.local>
2020-06-04 16:20:26 +0800

    First Commit

---
+++ first app/first app/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  first app
+//
+//  Created by @gastby on 2020/6/4.
+//  Copyright © 2020 @gastby. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
